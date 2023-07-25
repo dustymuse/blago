dcc9c781cbaac6649909361198c06fa3c54f03d3
Risha Rozenberg <teamofdk@gmail.com>
2023-07-25 15:05:06 +0300

    done new js in news

--- assets/styles/news.js
+++ assets/styles/news.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (lessButton) {
                     lessButton.style.display = "inline";
                 }
-                newsItem.style.marginBottom = "20px"; // Добавляем margin-bottom при появлении кнопок
+                newsItem.style.marginBottom = "20px";
                 localStorage.setItem(`news${newsItem.dataset.id}`, "true");
             });
 
@@ -45,10 +45,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     });
                     moreButton.style.display = "inline";
                     lessButton.style.display = "none";
-                    newsItem.style.marginBottom = "0"; // Удаляем margin-bottom при скрытии кнопок
+                    newsItem.style.marginBottom = "0";
                     localStorage.setItem(`news${newsItem.dataset.id}`, "false");
                 });
-
                 newsItem.appendChild(lessButton);
             }
 
@@ -63,8 +62,78 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (lessButton) {
                     lessButton.style.display = "inline";
                 }
-                newsItem.style.marginBottom = "20px"; // Устанавливаем margin-bottom при загрузке, если текст развернут
+                newsItem.style.marginBottom = "20px";
             }
         }
     });
 });
+
+//перенос на след.страницу-------------------------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", function () {
+    const newsItems = document.querySelectorAll(".new");
+    const newsLines = document.querySelectorAll(".news__line");
+    const itemsPerPage = 5; // Количество блоков new на одной странице
+    const pagination = document.querySelector(".pagination");
+    let currentPage = sessionStorage.getItem("currentPage") || 1;
+    let scrollPosition = sessionStorage.getItem("scrollPosition") || 0;
+
+    function showPage(pageNumber) {
+        const startIndex = (pageNumber - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+
+        newsItems.forEach(function (newsItem, index) {
+            if (index >= startIndex && index < endIndex) {
+                newsItem.style.display = "block";
+                newsLines[index].style.display = "block";
+            } else {
+                newsItem.style.display = "none";
+                newsLines[index].style.display = "none";
+            }
+        });
+
+        // После отображения страницы прокручиваем к элементу с классом for-slider
+        const forSlider = document.querySelector(".for-slider");
+        forSlider.scrollIntoView({ behavior: "smooth" });
+    }
+
+    function updatePaginationButtons() {
+        pagination.innerHTML = "";
+        for (let i = 1; i <= Math.ceil(newsItems.length / itemsPerPage); i++) {
+            const pageButton = document.createElement("button");
+            pageButton.textContent = i;
+            pageButton.className = "page-number";
+            pageButton.dataset.page = i;
+
+            if (i === parseInt(currentPage)) {
+                pageButton.disabled = true;
+                pageButton.classList.add("page-number_active");
+            } else {
+                pageButton.disabled = false;
+                pageButton.classList.remove("page-number_active");
+            }
+
+            pageButton.addEventListener("click", function () {
+                currentPage = i;
+                showPage(currentPage);
+                updatePaginationButtons();
+                sessionStorage.setItem("currentPage", currentPage);
+            });
+
+            pagination.appendChild(pageButton);
+        }
+    }
+
+    if (newsItems.length > itemsPerPage) {
+        pagination.style.display = "block";
+        showPage(currentPage);
+        updatePaginationButtons();
+    } else {
+        pagination.style.display = "none";
+    }
+});
+
+// Сохраняем текущую позицию страницы перед закрытием или переходом на другую страницу
+window.addEventListener("beforeunload", function () {
+    sessionStorage.setItem("scrollPosition", window.scrollY);
+});
+
