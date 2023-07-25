75be484d3b69edf00e80ca042673b34ffebf5d26
Risha Rozenberg <teamofdk@gmail.com>
2023-07-25 13:17:09 +0300

    add in js

--- assets/styles/news.js
+++ assets/styles/news.js
@@ -1,34 +1,70 @@
-//свернуть-развернуть-------------------------------------------
 document.addEventListener("DOMContentLoaded", function () {
-    const newsItems = document.querySelectorAll(".new-more");
+    const newsItems = document.querySelectorAll(".new");
 
     newsItems.forEach(function (newsItem) {
-        const moreButton = newsItem.querySelector(".more");
-        const hiddenParagraphs = newsItem.querySelector(".hidden-paragraphs");
-        const lessButton = newsItem.querySelector(".less");
+        const hiddenParagraphs = newsItem.querySelectorAll(".new__text");
+        const moreButton = document.createElement("p");
+        moreButton.className = "more";
+        moreButton.textContent = "Читать далее";
 
-        const isExpanded = localStorage.getItem(`news${newsItem.dataset.id}`) === "true";
-
-        if (isExpanded) {
-            hiddenParagraphs.style.display = "block";
+        if (hiddenParagraphs.length <= 2) {
+            hiddenParagraphs[hiddenParagraphs.length - 1].style.marginBottom = "0";
             moreButton.style.display = "none";
         } else {
-            hiddenParagraphs.style.display = "none";
-            lessButton.style.display = "none";
+            hiddenParagraphs.forEach(function (paragraph, index) {
+                if (index >= 2) {
+                    paragraph.style.display = "none";
+                }
+            });
+
+            moreButton.addEventListener("click", function () {
+                hiddenParagraphs.forEach(function (paragraph) {
+                    paragraph.style.display = "block";
+                });
+                moreButton.style.display = "none";
+                if (lessButton) {
+                    lessButton.style.display = "inline";
+                }
+                newsItem.style.marginBottom = "20px"; // Добавляем margin-bottom при появлении кнопок
+                localStorage.setItem(`news${newsItem.dataset.id}`, "true");
+            });
+
+            let lessButton = null;
+
+            if (hiddenParagraphs.length > 2) {
+                lessButton = document.createElement("p");
+                lessButton.className = "less";
+                lessButton.textContent = "Свернуть";
+                lessButton.style.display = "none";
+
+                lessButton.addEventListener("click", function () {
+                    hiddenParagraphs.forEach(function (paragraph, index) {
+                        if (index >= 2) {
+                            paragraph.style.display = "none";
+                        }
+                    });
+                    moreButton.style.display = "inline";
+                    lessButton.style.display = "none";
+                    newsItem.style.marginBottom = "0"; // Удаляем margin-bottom при скрытии кнопок
+                    localStorage.setItem(`news${newsItem.dataset.id}`, "false");
+                });
+
+                newsItem.appendChild(lessButton);
+            }
+
+            newsItem.appendChild(moreButton);
+
+            const isExpanded = localStorage.getItem(`news${newsItem.dataset.id}`) === "true";
+            if (isExpanded) {
+                hiddenParagraphs.forEach(function (paragraph) {
+                    paragraph.style.display = "block";
+                });
+                moreButton.style.display = "none";
+                if (lessButton) {
+                    lessButton.style.display = "inline";
+                }
+                newsItem.style.marginBottom = "20px"; // Устанавливаем margin-bottom при загрузке, если текст развернут
+            }
         }
-
-        moreButton.addEventListener("click", function () {
-            hiddenParagraphs.style.display = "block";
-            moreButton.style.display = "none";
-            lessButton.style.display = "inline";
-            localStorage.setItem(`news${newsItem.dataset.id}`, "true");
-        });
-
-        lessButton.addEventListener("click", function () {
-            hiddenParagraphs.style.display = "none";
-            moreButton.style.display = "inline";
-            lessButton.style.display = "none";
-            localStorage.setItem(`news${newsItem.dataset.id}`, "false");
-        });
     });
 });
